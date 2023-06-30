4dc11cb15ffee6c544139ba879a046517fc5070c
Fabian <burner.afire@gmail.com>
2023-06-29 21:17:40 -0600

    Added footer and their sytles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
     return () => {
       window.removeEventListener("scroll", controlNavbar)  
     } 
-  }, [])
+  }, [lastScrollY])
 
   const searchQueryHandler = (event) => {
     const query = searchInputRef.current.value

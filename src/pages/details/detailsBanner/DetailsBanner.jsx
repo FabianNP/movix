fd1d68f755e3290c218015f454e267b341fc89c0
Fabian <burner.afire@gmail.com>
2023-07-02 22:40:55 -0600

    Added description and crew on DetailBanner.jsx and change some styling about the navMenu and searchBar on header

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -20,6 +20,9 @@ const DetailsBanner = ({ video, crew }) => {
   const { url } = useSelector(state => state.home)
   const _genres = data?.genres.map(g => g.id)
 
+  const director = crew?.filter((f) => f.job === "Director")
+  const writer = crew?.filter((f) => f.job === "Screenplay" || "Story" || "Writer")
+
     const toHoursAndMinutes = (totalMinutes) => {
         const hours = Math.floor(totalMinutes / 60);
         const minutes = totalMinutes % 60;
@@ -54,7 +57,7 @@ const DetailsBanner = ({ video, crew }) => {
                             <div className="row">
                               <CircleRating rating={data.vote_average.toFixed(1)} />
                               <div className="playbtn" onClick={() => {
-                                
+
                               }}>
                                 <PlayIcon />
                                 <span className="text">
@@ -62,6 +65,74 @@ const DetailsBanner = ({ video, crew }) => {
                                 </span>
                               </div>
                             </div>
+                            <div className="overview">
+                              <div className="heading">Overview</div>
+                              <div className="description">{data.overview}</div>
+                            </div>
+                            <div className="info">
+                              {data.status && (
+                                <div className="infoItem">
+                                  <span className="text bold">
+                                    Status:{" "}
+                                  </span>
+                                  <span className="text">
+                                    {data.status}
+                                  </span>
+                                </div>
+                              )}
+                              {data.release_date && (
+                                <div className="infoItem">
+                                  <span className="text bold">
+                                    Release Date:{" "}
+                                  </span>
+                                  <span className="text">
+                                    {dayjs(data.release_date).format("MMMM D, YYYY")}
+                                  </span>
+                                </div>
+                              )}
+                              {data.runtime && (
+                                <div className="infoItem">
+                                  <span className="text bold">
+                                    Release Date:{" "}
+                                  </span>
+                                  <span className="text">
+                                    {toHoursAndMinutes(data.runtime)}
+                                  </span>
+                                </div>
+                              )}
+                            </div>
+                            {director?.length > 0 && (
+                              <div className="info">
+                                <span className="text bold">
+                                  Director:{" "}
+                                </span>
+                                <span className="text">
+                                  {director.map((d, i) => (
+                                    <span key={i}>
+                                      {d.name}
+                                      {director.length - 1 !== i && ", "}
+                                    </span>
+                                  ))}
+                                </span>
+                                
+                              </div>
+                            )}
+                            {writer?.length > 0 && (
+                              <div className="info">
+                                <span className="text bold">
+                                  Writter{writer.length - 1 > 0 ? "s" : ""}:{" "}
+                                </span>
+                                <span className="text">
+                                  {writer.slice(0, 5).map((d, i) => (
+                                    <span key={i}>
+                                      {d.name}
+                                      {writer.slice(0, 5).length - 1 !== i && ", "}
+                                    </span>
+                                  ))}
+                                </span>
+                                
+                              </div>
+                            )}
                           </div>
                         </div>
                       </ContentWrapper>

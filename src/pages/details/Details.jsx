dde58e6b92889a96982f4192be9c6990b3744c1b
Fabian <burner.afire@gmail.com>
2023-07-03 16:14:07 -0600

    Finished the Details Banner for the movie with all carousels and information

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./style.scss"
 import useFetch from "../../hooks/useFetch"
 import { useParams } from 'react-router-dom'
 import DetailsBanner from './detailsBanner/DetailsBanner'
+import Cast from './cast/Cast'
+import VideosSection from './videosSection/VideosSection'
+import Similar from '../carousels/Similar'
+import Recommendation from '../carousels/Recommendation'
+
 
 const Details = () => {
   const { mediaType, id } = useParams()
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`)
   const { data: credits, loading: credistLoading } = useFetch(`/${mediaType}/${id}/credits`)
+  
+  // const officialTrailer = data?.filter(v => v.name === "Official Trailer")
+
 
   return (
     <div>
       <DetailsBanner video={data?.results[0]} crew={credits?.crew}/>
+      <Cast data={credits?.cast} loading={credistLoading} />
+      <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id }/>
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   )
 }
--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import dayjs from "dayjs";
@@ -11,152 +11,177 @@ import Genres from "../../../components/genres/Genres";
 import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/lazyLoadImage/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
-import  { PlayIcon } from "./Playbtn"
+import { PlayIcon } from "./Playbtn";
+import VideoPopup from "../../../components/videoPopup/VideoPopup";
 
 const DetailsBanner = ({ video, crew }) => {
-  const { mediaType, id } = useParams()
-  const { data, loading } = useFetch(`/${mediaType}/${id}`)
+  const [show, setShow] = useState(false);
+  const [videoId, setVideoId] = useState(null);
 
-  const { url } = useSelector(state => state.home)
-  const _genres = data?.genres.map(g => g.id)
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}`);
 
-  const director = crew?.filter((f) => f.job === "Director")
-  const writer = crew?.filter((f) => f.job === "Screenplay" || "Story" || "Writer")
+  const { url } = useSelector((state) => state.home);
+  const _genres = data?.genres.map((g) => g.id);
 
-    const toHoursAndMinutes = (totalMinutes) => {
-        const hours = Math.floor(totalMinutes / 60);
-        const minutes = totalMinutes % 60;
-        return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
-    };
+  const director = crew?.filter((f) => f.job === "Director");
+  const writer = crew?.filter(
+    (f) => f.job === "Screenplay" || "Story" || "Writer"
+  );
 
-    return (
-        <div className="detailsBanner">
-            {!loading ? (
-                <>
-                  {!!data && (
-                    <React.Fragment>
-                      <div className="backdrop-img">
-                        <Img src={url.backdrop + data.backdrop_path} />
+  // const officialTrailer = useRef()
+  // useEffect(() => {
+  //   officialTrailer.current = video.filter(v => v.name == "Official Trailer")
+  // }, [video])
+
+  const toHoursAndMinutes = (totalMinutes) => {
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
+  };
+
+  return (
+    <div className="detailsBanner">
+      {!loading ? (                         //--------Ternary Operator 1
+        <>
+          {!!data && (
+            <React.Fragment>                 
+              <div className="backdrop-img">
+                <Img src={url.backdrop + data.backdrop_path} />
+              </div>
+              <div className="opacity-layer"></div>
+              <ContentWrapper> 
+                <div className="content">
+                  <div className="left">
+                    {data.poster_path ? (
+                      <Img
+                        className="posterImg"
+                        src={url.backdrop + data.poster_path}
+                      />
+                    ) : (
+                      <Img className="posterImg" src={PosterFallback} />
+                    )}
+                  </div>
+                  <div className="right">
+                    <div className="title">
+                      {`${data.name || data.title}(${dayjs(
+                        data?.release_date
+                      ).format("YYYY")})`}
+                    </div>
+                    <div className="subtitle">{data.tagline}</div>
+                    <Genres data={_genres} />
+                    <div className="row">
+                      <CircleRating rating={data.vote_average.toFixed(1)} />
+                      <div
+                        className="playbtn"
+                        onClick={() => {
+                          setShow(true);
+                          setVideoId(video.key);
+                        }}
+                      >
+                        <PlayIcon />
+                        <span className="text">Watch Trailer</span>
                       </div>
-                      <div className="opacity-layer"></div>
-                      <ContentWrapper>
-                        <div className="content">
-                          <div className="left">
-                            {data.poster_path ? 
-                              <Img className="posterImg" src={url.backdrop + data.poster_path}/> : 
-                              <Img className="posterImg" src={PosterFallback}/>}
-                          </div>
-                          <div className="right">
-                            <div className="title">
-                              {`${data.name || data.title}(${dayjs(data?.release_date).format("YYYY")})`}
-                            </div>
-                            <div className="subtitle">
-                              {data.tagline}
-                            </div>
-                            <Genres data={_genres} />
-                            <div className="row">
-                              <CircleRating rating={data.vote_average.toFixed(1)} />
-                              <div className="playbtn" onClick={() => {
-
-                              }}>
-                                <PlayIcon />
-                                <span className="text">
-                                  Watch Trailer
-                                </span>
-                              </div>
-                            </div>
-                            <div className="overview">
-                              <div className="heading">Overview</div>
-                              <div className="description">{data.overview}</div>
-                            </div>
-                            <div className="info">
-                              {data.status && (
-                                <div className="infoItem">
-                                  <span className="text bold">
-                                    Status:{" "}
-                                  </span>
-                                  <span className="text">
-                                    {data.status}
-                                  </span>
-                                </div>
-                              )}
-                              {data.release_date && (
-                                <div className="infoItem">
-                                  <span className="text bold">
-                                    Release Date:{" "}
-                                  </span>
-                                  <span className="text">
-                                    {dayjs(data.release_date).format("MMMM D, YYYY")}
-                                  </span>
-                                </div>
-                              )}
-                              {data.runtime && (
-                                <div className="infoItem">
-                                  <span className="text bold">
-                                    Release Date:{" "}
-                                  </span>
-                                  <span className="text">
-                                    {toHoursAndMinutes(data.runtime)}
-                                  </span>
-                                </div>
-                              )}
-                            </div>
-                            {director?.length > 0 && (
-                              <div className="info">
-                                <span className="text bold">
-                                  Director:{" "}
-                                </span>
-                                <span className="text">
-                                  {director.map((d, i) => (
-                                    <span key={i}>
-                                      {d.name}
-                                      {director.length - 1 !== i && ", "}
-                                    </span>
-                                  ))}
-                                </span>
-                                
-                              </div>
-                            )}
-                            {writer?.length > 0 && (
-                              <div className="info">
-                                <span className="text bold">
-                                  Writter{writer.length - 1 > 0 ? "s" : ""}:{" "}
-                                </span>
-                                <span className="text">
-                                  {writer.slice(0, 5).map((d, i) => (
-                                    <span key={i}>
-                                      {d.name}
-                                      {writer.slice(0, 5).length - 1 !== i && ", "}
-                                    </span>
-                                  ))}
-                                </span>
-                                
-                              </div>
-                            )}
-                          </div>
+                    </div>
+                    <div className="overview">
+                      <div className="heading">Overview</div>
+                      <div className="description">{data.overview}</div>
+                    </div>
+                    <div className="info">
+                      {data.status && (
+                        <div className="infoItem">
+                          <span className="text bold">Status: </span>
+                          <span className="text">{data.status}</span>
                         </div>
-                      </ContentWrapper>
-                    </React.Fragment>
-                  )}
-                </>
-            ) : (
-                <div className="detailsBannerSkeleton">
-                    <ContentWrapper>
-                        <div className="left skeleton"></div>
-                        <div className="right">
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
+                      )}
+                      {data.release_date && (
+                        <div className="infoItem">
+                          <span className="text bold">Release Date: </span>
+                          <span className="text">
+                            {dayjs(data.release_date).format("MMMM D, YYYY")}
+                          </span>
                         </div>
-                    </ContentWrapper>
+                      )}
+                      {data.runtime && (
+                        <div className="infoItem">
+                          <span className="text bold">Release Date: </span>
+                          <span className="text">
+                            {toHoursAndMinutes(data.runtime)}
+                          </span>
+                        </div>
+                      )}
+                    </div>
+                    {director?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Director: </span>
+                        <span className="text">
+                          {director.map((d, i) => (
+                            <span key={i}>
+                              {d.name}
+                              {director.length - 1 !== i && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
+                    {writer?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">
+                          Writer{writer.length - 1 > 0 ? "s" : ""}:{" "}
+                        </span>
+                        <span className="text">
+                          {writer.slice(0, 5).map((d, i) => (
+                            <span key={i}>
+                              {d.name}
+                              {writer.slice(0, 5).length - 1 !== i && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
+                    {data?.created_by?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Creator{data}: </span>
+                        <span className="text">
+                          {data.created_by?.map((d, i) => (
+                            <span key={i}>
+                              {d.name}
+                              {data.length - 1 !== i && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
+                  </div>
                 </div>
-            )}
+                <VideoPopup
+                  show={show}
+                  setShow={setShow}
+                  videoId={videoId}
+                  setVideoId={setVideoId}
+                />
+              </ContentWrapper>
+            </React.Fragment>
+          )}
+        </>
+      ) : (                                    //--------Ternary Operator 2
+        <div className="detailsBannerSkeleton">
+          <ContentWrapper>
+            <div className="left skeleton"></div>
+            <div className="right">
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+            </div>
+          </ContentWrapper>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default DetailsBanner;
